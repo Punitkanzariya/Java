956a66ab5f58d3353e2335c4eb9b6de4d1a2831a
Punit Kanzariya <punitkanzariya4706@gmail.com>
2025-01-26 20:17:08 +0530

    Create Pallingdron.java

---
+++ Pallingdron.java
@@ -0,0 +1,22 @@
+// check for given string or number Pallingdron or not
+import java.util.*
+;
+public class Pallingdron{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String str1="", str2="";
+        System.out.println("Enter your String or Number:");
+        str1=in.nextLine();
+
+        int strLength = str1.length();
+        for (int i = (strLength - 1); i >=0; --i) {
+          str2 = str2 + str1.charAt(i);
+        }
+        if(str1.equals(str2)){
+            System.out.println(str1+" String or number is Pallingdron");
+        }
+        else{
+            System.out.println(str1+" String or number is not Pallingdron");
+        }
+    }
+}
