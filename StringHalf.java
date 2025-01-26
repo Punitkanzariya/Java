6f6b3e33e604c072b3f0209d3be20cde43cb5b56
Punit Kanzariya <punitkanzariya4706@gmail.com>
2025-01-26 19:59:33 +0530

    Create StringHalf.java

---
+++ StringHalf.java
@@ -0,0 +1,11 @@
+// java program for find length of string and print second half of string
+
+public class StringHalf{
+    public static void main(String[] args){
+        String str="Punit kanzariya";
+        int length = str.length();
+        int i = length / 2;
+        System.out.println("Length of string:"+length);
+        System.out.println("Second Half of string: "+str.substring(i));
+    }
+}
