7ed1b0904b4912265ec2f7ce36be21a3948e5c63
Punit Kanzariya <punitkanzariya4706@gmail.com>
2025-01-26 20:01:03 +0530

    Update StringHalf.java

--- StringHalf.java
+++ StringHalf.java
@@ -5,6 +5,7 @@ public class StringHalf{
         String str="Punit kanzariya";
         int length = str.length();
         int i = length / 2;
+        System.out.println("Main string:"+str);
         System.out.println("Length of string:"+length);
         System.out.println("Second Half of string: "+str.substring(i));
     }
